b096af97cc1f62636840d4d3b9f29cd4dedce7b8
Kees van Drongelen <kees@visualspace.nl>
2020-12-09 10:18:08 +0100

    Video tweaks and some more debugging

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -19,13 +19,12 @@
             class="px-0 d-flex justify-space-between align-center"
           >
             <div
-              class="ml-2 d-none d-lg-block"
-              style="min-width: 200px"
+              class="ml-2"
             >
               <div
                 class="d-flex align-center"
                 @click="goHome"
-                style="cursor: pointer;"
+                style="cursor: pointer; min-width: 34px;"
               >
                 <v-img
                   alt="DWEB Search Logo"
@@ -35,7 +34,7 @@
                   width="32"
                   height="32"
                 />
-                <h2 dark class="white--text">DWEB <span>SEARCH</span></h2>
+                <h2 dark class="white--text d-none d-lg-block">DWEB <span>SEARCH</span></h2>
               </div>
             </div>
             <div class="flex-grow-1">
@@ -57,17 +56,16 @@
       via a click 'on'. The menu it self consists of list components
       containing list-items which contains a list-item-title
     -->
-    <v-container
-    >
+    <v-container>
       <v-row>
-        <v-col cols="12" xl="8" offset-xl="2" align="end" class="text--secondary body-2">
-          <span class="hidden-sm-and-down">Filter: </span>
+        <v-col cols="12" xl="8" offset-xl="2" align="end" class="text--secondary text-body-2">
+          <span class="hidden-sm-and-down text-body-2">Filter: </span>
 
           <span>
             <v-menu>
               <template v-slot:activator="{ on, attrs }">
                 <span
-                  class="m-2"
+                  class="m-2 text-body-2"
                   v-on="on"
                   v-bind="attrs"
                 >
@@ -80,7 +78,7 @@
                   :key="index"
                   @click="setSizeFilter(index)"
                 >
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <v-list-item-title class="text-body-2">{{ item.title }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -90,7 +88,7 @@
             <v-menu>
               <template v-slot:activator="{ on, attrs }">
                 <span
-                  class="m-2"
+                  class="m-2 text-body-2"
                   v-on="on"
                   v-bind="attrs"
                 >
@@ -106,7 +104,7 @@
                   :key="index"
                   @click="setLastSeenFilter(index)"
                 >
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <v-list-item-title class="text-body-2">{{ item.title }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -135,10 +133,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="green--text">Last seen 1 day ago</span><span> | Size 478mb</span><span> | Mimetype text/html</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Unlimited music now
             </v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums whenever and wherever, online and offline albums whenever and wherever, online and offline.</v-card-subtitle>
           </v-card>
         </v-col>
         <v-col cols="12" xl="8" offset-xl="2">
@@ -146,10 +144,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="orange--text">Last seen 1 week ago</span><span> | Size 478mb</span><span> | Mimetype audio/mpeg</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Unlimited music now
             </v-card-title>
-            <v-card-subtitle>
+            <v-card-subtitle class="text-body-2">
               Listen to your favorite artists and albums whenever and wherever, online and offline.
             </v-card-subtitle>
           </v-card>
@@ -159,10 +157,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="red--text">Last seen 2 months ago</span><span> | Size 478mb</span> <span> | Mimetype application/msword</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Unlimited music now
             </v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
@@ -231,7 +229,7 @@
           >
             <v-col
               v-for="card in cards"
-              :key="card.title"
+              :key="card.id"
               cols="6" xs="4" sm="4" md="3" lg="2"
             >
               <v-card
@@ -241,11 +239,10 @@
                   class="white--text align-end"
                   :aspect-ratio="1"
                   gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  :width="width"
                 >
                   <v-icon size="64" color="white" style="opacity: 0.3; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-music</v-icon>
-                  <v-card-title class="subtitle-1 font-weight-bold" v-text="card.title"></v-card-title>
-                  <v-card-subtitle class="white--text text-caption" v-text="card.subtitle"></v-card-subtitle>
+                  <v-card-title class="text-subtitle-1 font-weight-bold" v-text="card.title"></v-card-title>
+                  <v-card-subtitle class="white--text text-body-2" v-text="card.subtitle"></v-card-subtitle>
                 </v-img>
 
                 <v-card-subtitle class="text-caption text-truncate">
@@ -272,73 +269,62 @@
         </v-col>
       </v-row>
 
-
       <v-row dense>
-        <!-- <v-col cols="12" xl="8" offset-xl="2">
-          <v-card
-            @click="genericDialog()"
-          >
-            <div class="d-flex flex-no-wrap justify-start">
-              <div>
-                <v-avatar
-                  class="ma-3 mr-0"
-                  width="179"
-                  height="96"
-                  tile
+
+        <v-col cols="12" xl="8" offset-xl="2">
+          <v-card>
+            <v-row>
+              <v-col cols="12" sm="4" md="3" lg="2" class="py-0">
+                <v-img
+                  class="ma-3 mr-sm-0"
+                  cover
+                  aspect-ratio="1.778"
+                  src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                 >
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  >
-                    <v-icon size="64" color="white" style="opacity: 0.9; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-play</v-icon>
-                  </v-img>
-                </v-avatar>
-              </div>
-              <div class="mt-n2">
+                  <v-icon size="64" color="white" style="opacity: 0.9; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-play</v-icon>
+                </v-img>
+              </v-col>
+              <v-col cols="12" sm="8" md="9" lg="10" class="mt-sm-n2 mt-n7 py-sm-0 ml-sm-n6">
                 <v-card-subtitle class="text-caption mb-n7 text-truncate">
                   <span class="green--text">Last seen 1 day ago</span><span> | Size 478mb</span>
                 </v-card-subtitle>
-                <v-card-title class="subtitle-1">
+                <v-card-title class="text-subtitle-1">
                   Unlimited music now
                 </v-card-title>
-                <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline online and offline.</v-card-subtitle>
-              </div>
-            </div>
+                <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums artists and albums whenever and wherever, online and online and offline.</v-card-subtitle>
+              </v-col>
+            </v-row>
           </v-card>
         </v-col>
 
         <v-col cols="12" xl="8" offset-xl="2">
           <v-card>
-            <div class="d-flex flex-no-wrap justify-start">
-              <div>
-                <v-avatar
-                  class="ma-3 mr-0"
-                  width="179"
-                  height="96"
-                  tile
+            <v-row>
+              <v-col cols="12" sm="4" md="3" lg="2" class="py-0">
+                <v-img
+                  class="ma-3 mr-sm-0"
+                  cover
+                  aspect-ratio="1.778"
+                  src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                 >
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  >
-                    <v-icon size="64" color="white" style="opacity: 0.9; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-play</v-icon>
-                  </v-img>
-                </v-avatar>
-              </div>
-              <div class="mt-n2">
+                  <v-icon size="64" color="white" style="opacity: 0.9; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-play</v-icon>
+                </v-img>
+              </v-col>
+              <v-col cols="12" sm="8" md="9" lg="10" class="mt-sm-n2 mt-n7 py-sm-0 ml-sm-n6">
                 <v-card-subtitle class="text-caption mb-n7 text-truncate">
                   <span class="orange--text">Last seen 1 week ago</span><span> | Size 478mb</span>
                 </v-card-subtitle>
-                <v-card-title class="subtitle-1">
+                <v-card-title class="text-subtitle-1">
                   Unlimited music now
                 </v-card-title>
-                <v-card-subtitle>
-                  Listen to your favorite artists and albums whenever and artists and albums whenever and wherever, onlineonline and offline.
-                </v-card-subtitle>
-              </div>
-            </div>
+                <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums artists and albums whenever and wherever, online and online and offline.</v-card-subtitle>
+              </v-col>
+            </v-row>
           </v-card>
-        </v-col> -->
+        </v-col>
+
 
         <v-col cols="12" xl="8" offset-xl="2">
           <v-card>
@@ -358,53 +344,17 @@
                 <v-card-subtitle class="text-caption mb-n7 text-truncate">
                   <span class="red--text">Last seen 2 months ago</span><span> | Size 478mb</span>
                 </v-card-subtitle>
-                <v-card-title class="subtitle-1">
+                <v-card-title class="text-subtitle-1">
                   Unlimited music now
                 </v-card-title>
-                <v-card-subtitle>Listen to your favorite artists and albums artists and albums whenever and wherever, online and online and offline.</v-card-subtitle>
+                <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums artists and albums whenever and wherever, online and online and offline.</v-card-subtitle>
               </v-col>
             </v-row>
-
           </v-card>
         </v-col>
+
       </v-row>
 
-      <!-- <v-row dense>
-        <v-col cols="12" xl="8" offset-xl="2">
-          <v-row
-            dense
-          >
-            <v-col
-              v-for="card in cards"
-              :key="card.title"
-              cols="6" xs="4" sm="4" md="3" lg="2"
-            >
-              <v-card
-                flat
-                shaped
-                tile
-              >
-                <v-img
-                  :src="card.src"
-                  class="white--text align-end"
-                  :aspect-ratio="16/9"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  :width="width"
-                >
-                  <v-icon size="64" color="white" style="opacity: 0.9; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">mdi-play</v-icon>
-                </v-img>
-
-                <v-card-subtitle class="text-caption text-truncate">
-                  <div class="my-n2">
-                    <span class="red--text">Last seen 2 months ago</span><br>
-                    <span>Size 478mb</span>
-                  </div>
-                </v-card-subtitle>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row> -->
     </v-container>
 
 
@@ -426,10 +376,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="green--text">Last seen 1 day ago</span><span> | Size 478mb</span><span> | 128 files</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Beatles samples and works
             </v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums whenever and wherever, online and offline albums whenever and wherever, online and offline.</v-card-subtitle>
           </v-card>
         </v-col>
         <v-col cols="12" xl="8" offset-xl="2">
@@ -437,10 +387,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="orange--text">Last seen 1 week ago</span><span> | Size 478mb</span><span> | 528 files</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Unlimited music now
             </v-card-title>
-            <v-card-subtitle>
+            <v-card-subtitle class="text-body-2">
               Listen to your favorite artists and albums whenever and wherever, online and offline.
             </v-card-subtitle>
           </v-card>
@@ -450,10 +400,10 @@
             <v-card-subtitle class="text-caption mb-n7 text-truncate">
               <span class="red--text">Last seen 2 months ago</span><span> | Size 478mb</span> <span> | 64 files</span>
             </v-card-subtitle>
-            <v-card-title class="subtitle-1">
+            <v-card-title class="text-subtitle-1">
               Beatles All - Files
             </v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-subtitle class="text-body-2">Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
@@ -506,12 +456,12 @@ export default {
     ],
 
     cards: [
-      { title: 'Beatles', subtitle: 'Hey Jude', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
-      { title: 'Beatles', subtitle: 'Yesterday.mp3', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
-      { title: 'Beatles', subtitle: 'The White Album', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      { title: 'Beatles', subtitle: 'Goodnight.mp3', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
-      { title: 'Beatles', subtitle: 'The White Album 2', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      { title: 'Beatles', subtitle: 'The wallrus', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+      { id: "1", title: 'Beatles', subtitle: 'Hey Jude', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+      { id: "2", title: 'Beatles', subtitle: 'Yesterday.mp3', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+      { id: "3", title: 'Beatles', subtitle: 'The White Album', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+      { id: "4", title: 'Beatles', subtitle: 'Goodnight.mp3', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+      { id: "5", title: 'Beatles', subtitle: 'The White Album 2', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+      { id: "6", title: 'Beatles', subtitle: 'The wallrus', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
     ],
   }),
 
@@ -560,11 +510,11 @@ export default {
   mounted() {
     // Once we enter this view we want retrieve data from the api with
     // the params from the given url
-    const query = this.$route.query.query;
-    const type = this.$route.query.type;
-    if (query) {
-      this.getDataFromApi(query, type);
-    }
+    // const query = this.$route.query.query;
+    // const type = this.$route.query.type;
+    // if (query) {
+    //   this.getDataFromApi(query, type);
+    // }
   }
 }
 </script>
