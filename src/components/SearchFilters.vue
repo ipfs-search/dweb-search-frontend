d97a9afaf983cd6319da65cb184f433a5a6e6361
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-04-23 11:47:25 +0100

    Standard component for filters.

--- src/components/SearchFilters.vue
+++ src/components/SearchFilters.vue
@@ -10,36 +10,47 @@
 
           <v-row>
             <v-col>
-              <!-- <span class="hidden-sm-and-down text-body-2">Filter: </span> -->
+              <!-- TODO: Align me properly -->
+              <span class="hidden-sm-and-down text-body-2">Filter: </span>
             </v-col>
 
             <v-col cols="2">
+              <!--
+                Select component has everything setup for smooth interaction (including setting values).
+
+                I strongly suggest we do everything we can to avoid creating custom components; it causes A LOT of
+                extra work on the implementation side (= making things work, well, including edge cases, keyboard
+                support, accessibility etc.) If it is absolutely necessary we can always do that later.
+
+                The select component wraps a menu component, so we can set any properties, possibly even override
+                template slots etc.
+
+                Ref:
+                https://vuetifyjs.com/en/components/selects/#menu-props
+                https://github.com/vuetifyjs/vuetify/tree/master/packages/vuetify/src/components/VSelect
+              -->
               <v-select
                 :items="sizeOptions"
-                dense
+                dense solo
                 label="Size"
-              />
+              >
+                <template v-slot:selection="{ item }">
+                  <span class="m-2 text-body-2">Size {{ item.text }}</span>
+                </template>
+              </v-select>
             </v-col>
 
             <v-col cols="2">
               <v-select
                 :items="lastSeenOptions"
-                dense
+                dense solo
                 label="Last seen"
-              />
+              >
+                <template v-slot:selection="{ item }">
+                  <span class="m-2 text-body-2">Last seen {{ item.text }}</span>
+                </template>
+              </v-select>
             </v-col>
-<!--
-            <SearchFilter
-              label="Last seen"
-              :options="sizeOptions"
-              v-model="sizeFilter"
-            />
-
-            <SearchFilter
-              label="Last seen"
-              :options="lastSeenOptions"
-              v-model="lastSeenFilter"
-            /> -->
           </v-row>
 
         </v-col>
