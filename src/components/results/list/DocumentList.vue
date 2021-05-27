b2e84bc6c766f83ba077d14be4c37cc8e5a80ea1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-05-27 12:03:39 +0100

    Towards generic list component.

--- src/components/results/list/DocumentList.vue
+++ src/components/results/list/DocumentList.vue
@@ -1,92 +1,80 @@
 <template>
-  <v-container>
-    <v-row dense>
-      <v-col
-        cols="12"
-        xl="8"
-        offset-xl="2"
-      >
-        <v-subheader class="text-subtitle-1 mt-n2 mb-n3 d-flex justify-space-between">
-          <div>
-            Texts (230)
-          </div>
-          <div>
-            <a class="text-subtitle-1 text-decoration-none text--secondary">
-              View all
-            </a>
-          </div>
-        </v-subheader>
-      </v-col>
-    </v-row>
+  <ListBase>
+    <template v-slot:type>
+      Documents ({{ results.hits.length }})
+    </template>
 
-    <v-row dense>
-      <v-col
-        cols="12"
-        xl="8"
-        offset-xl="2"
+    <v-col
+      cols="12"
+      xl="8"
+      offset-xl="2"
+    >
+      <v-card
+        @click="genericDialog()"
       >
-        <v-card
-          @click="genericDialog()"
-        >
-          <v-card-subtitle class="text-caption mb-n7 text-truncate">
-            <span class="green--text">Last seen 1 day ago</span>
-            <span> | Size 478mb</span><span> | Mimetype text/html</span>
-          </v-card-subtitle>
-          <v-card-title class="text-subtitle-1">
-            Unlimited music now
-          </v-card-title>
-          <v-card-subtitle class="text-body-2">
-            Listen to your favorite artists and albums whenever and wherever, online and offline
-            albums whenever and wherever, online and offline.
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        xl="8"
-        offset-xl="2"
+        <v-card-subtitle class="text-caption mb-n7 text-truncate">
+          <span class="green--text">Last seen 1 day ago</span>
+          <span> | Size 478mb</span><span> | Mimetype text/html</span>
+        </v-card-subtitle>
+        <v-card-title class="text-subtitle-1">
+          Unlimited music now
+        </v-card-title>
+        <v-card-subtitle class="text-body-2">
+          Listen to your favorite artists and albums whenever and wherever, online and offline
+          albums whenever and wherever, online and offline.
+        </v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      xl="8"
+      offset-xl="2"
+    >
+      <v-card
+        @click="genericDialog()"
       >
-        <v-card
-          @click="genericDialog()"
-        >
-          <v-card-subtitle class="text-caption mb-n7 text-truncate">
-            <span class="orange--text">Last seen 1 week ago</span>
-            <span> | Size 478mb</span><span> | Mimetype audio/mpeg</span>
-          </v-card-subtitle>
-          <v-card-title class="text-subtitle-1">
-            Unlimited music now
-          </v-card-title>
-          <v-card-subtitle class="text-body-2">
-            Listen to your favorite artists and albums whenever and wherever, online and offline.
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        xl="8"
-        offset-xl="2"
+        <v-card-subtitle class="text-caption mb-n7 text-truncate">
+          <span class="orange--text">Last seen 1 week ago</span>
+          <span> | Size 478mb</span><span> | Mimetype audio/mpeg</span>
+        </v-card-subtitle>
+        <v-card-title class="text-subtitle-1">
+          Unlimited music now
+        </v-card-title>
+        <v-card-subtitle class="text-body-2">
+          Listen to your favorite artists and albums whenever and wherever, online and offline.
+        </v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      xl="8"
+      offset-xl="2"
+    >
+      <v-card
+        @click="genericDialog()"
       >
-        <v-card
-          @click="genericDialog()"
-        >
-          <v-card-subtitle class="text-caption mb-n7 text-truncate">
-            <span class="red--text">Last seen 2 months ago</span>
-            <span> | Size 478mb</span> <span> | Mimetype application/msword</span>
-          </v-card-subtitle>
-          <v-card-title class="text-subtitle-1">
-            Unlimited music now
-          </v-card-title>
-          <v-card-subtitle class="text-body-2">
-            Listen to your favorite artists and albums whenever and wherever, online and offline.
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        <v-card-subtitle class="text-caption mb-n7 text-truncate">
+          <span class="red--text">Last seen 2 months ago</span>
+          <span> | Size 478mb</span> <span> | Mimetype application/msword</span>
+        </v-card-subtitle>
+        <v-card-title class="text-subtitle-1">
+          Unlimited music now
+        </v-card-title>
+        <v-card-subtitle class="text-body-2">
+          Listen to your favorite artists and albums whenever and wherever, online and offline.
+        </v-card-subtitle>
+      </v-card>
+    </v-col>
+  </ListBase>
 </template>
 
 <script>
+import ListBase from './ListBase.vue';
+
 export default {
+  components: {
+    ListBase,
+  },
   props: {
     results: {
       type: Object,
