ea6aca916deb06b2bd51e3ea7009aaa4bc0dbb30
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-05-27 15:45:49 +0100

    Wrap image by card for proper cursor indication.

--- src/components/results/list/ImageList.vue
+++ src/components/results/list/ImageList.vue
@@ -19,26 +19,27 @@
           md="3"
           lg="2"
         >
-          <v-img
-            @click="genericDialog()"
-            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-            aspect-ratio="1"
-            class="grey lighten-2"
-          >
-            <template v-slot:placeholder>
-              <v-row
-                class="fill-height ma-0"
-                align="center"
-                justify="center"
-              >
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                />
-              </v-row>
-            </template>
-          </v-img>
+          <v-card @click="genericDialog()">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+                >
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  />
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
         </v-col>
       </v-row>
     </v-col>
