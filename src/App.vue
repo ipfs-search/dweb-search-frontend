d5f3fb891a426885db4671456e8c9ba48d36d9fe
Kees van Drongelen <kees@visualspace.nl>
2021-06-23 13:12:09 +0200

    Added style to solve bug with application height in Safari on iPhone

--- src/App.vue
+++ src/App.vue
@@ -62,3 +62,16 @@ export default {
     }
   }
 </style>
+
+<style lang="scss">
+  /*
+    This is due to a bug with the application height in Safari on IPhone
+    https://stackoverflow.com/questions/62167456/use-of-v-app-bar-causes-v-content-to-always-have-an-overflow-identical-to-the-he
+  */
+  @supports (-webkit-touch-callout: none) {
+    /* CSS specific to iOS devices */
+    div.v-application--wrap {
+      min-height:calc(100vh - 56px) !important;
+    }
+  }
+</style>
