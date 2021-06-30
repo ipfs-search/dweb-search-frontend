0c03078d36ee43f157565698c84541d43751986a
Kees van Drongelen <kees@visualspace.nl>
2021-06-30 14:59:06 +0200

    Added humanized format for sizes and duration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import VueTimeago from 'vue-timeago';
+import vueFilterPrettyBytes from 'vue-filter-pretty-bytes';
 import VBodyScrollLock from 'v-body-scroll-lock';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
@@ -6,6 +8,19 @@ import router from './router';
 import store from './store';
 
 Vue.use(VBodyScrollLock);
+
+Vue.use(VueTimeago, {
+  name: 'Timeago', // Component name, `Timeago` by default
+  locale: 'en', // Default locale
+  // We use `date-fns` under the hood
+  // So you can use all locales from it
+  // locales: {
+  //   'zh-CN': require('date-fns/locale/zh_cn'),
+  //   ja: require('date-fns/locale/ja')
+  // }
+});
+
+Vue.use(vueFilterPrettyBytes);
 
 Vue.config.productionTip = false;
 
