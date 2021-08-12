b47919b30214c48b805861cd7c03e5870205ade9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-08-12 15:35:52 +0100

    Dispatch search also on first load.

--- src/mixins/SearchNavigationMixin.js
+++ src/mixins/SearchNavigationMixin.js
@@ -6,37 +6,43 @@ export default {
   mounted() {
     // Set vuex query from route query parameters
     store.commit('query/setRouteParams', this.$route.query);
+    this.dispatch_search();
   },
 
+  methods: {
+    dispatch_search() {
+      // TODO: Move to store, perhaps.
+      // This logic might belong in store.
+      switch (store.state.query.type) {
+        // TODO: Use modular/centralised type list here!
+        case 'text':
+          store.dispatch('results/text/getResults');
+          break;
+        case 'images':
+          store.dispatch('results/images/getResults');
+          break;
+        case 'audio':
+          store.dispatch('results/audio/getResults');
+          break;
+        case 'video':
+          store.dispatch('results/video/getResults');
+          break;
+        case 'directories':
+          store.dispatch('results/video/getResults');
+          break;
+        default:
+          // 'any'
+          store.dispatch('results/text/getResults');
+          store.dispatch('results/images/getResults');
+          store.dispatch('results/audio/getResults');
+          store.dispatch('results/video/getResults');
+          store.dispatch('results/directories/getResults');
+      }
+    },
+  },
   beforeRouteUpdate(to, from, next) {
     // Perform search after update
-    // BUG: Doesn't get called on first page load
-    // This logic might belong in store.
-    switch (store.state.query.type) {
-      // TODO: Use modular/centralised type list here!
-      case 'text':
-        store.dispatch('results/text/getResults');
-        break;
-      case 'images':
-        store.dispatch('results/images/getResults');
-        break;
-      case 'audio':
-        store.dispatch('results/audio/getResults');
-        break;
-      case 'video':
-        store.dispatch('results/video/getResults');
-        break;
-      case 'directories':
-        store.dispatch('results/video/getResults');
-        break;
-      default:
-        // 'any'
-        store.dispatch('results/text/getResults');
-        store.dispatch('results/images/getResults');
-        store.dispatch('results/audio/getResults');
-        store.dispatch('results/video/getResults');
-        store.dispatch('results/directories/getResults');
-    }
+    this.dispatch_search();
 
     next();
   },
