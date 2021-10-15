cc17a5cc954b30cafc01b2c70a52b7e8b799e273
Frido Emans <verderster@gmail.com>
2021-10-15 14:38:34 +0200

    remove obsolete file and add todo

--- src/mixins/SearchMixin.js
+++ src/mixins/SearchMixin.js
@@ -21,6 +21,7 @@ export default {
 
   methods: {
     /**
+     * // TODO: move this file to helper and remove this file
      * push new route query and execute search lookup with page=0
      * should be called when a change to the search parameters is requested
      * @param newQuery
