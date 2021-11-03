c31484244d5b83f44bd369a9c4189a483d1f8cd7
Frido <frido@emans.co>
2021-11-03 23:05:20 +0100

    fix issue with duplicate resultsi; now carousel index is a simple index

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -71,6 +71,7 @@
         hide-delimiters
         hide-delimiter-background
         :continuous="false"
+        @change="onCarouselIndexChange"
       >
         <v-carousel-item
           v-for="(item, index) in items"
@@ -99,12 +100,29 @@ import DocumentDetail from '../components/results/detail/DocumentDetail';
 import DirectoryDetail from '../components/results/detail/DirectoryDetail';
 import VideoDetail from '../components/results/detail/VideoDetail';
 import AudioDetail from '../components/results/detail/AudioDetail';
-import { apiSearch } from '../helpers/ApiHelper';
 
 export default {
   beforeCreate() {
     store.commit('query/setRouteParams', this.$route.query);
     this.primaryPage = Number(this.$route.query.page) || 0;
+  },
+  created() {
+    this.$data.carouselIndex = this.items.findIndex((item) => item.hash === this.fileHash);
+    /*
+    // fetch requested detail page directly from the api if not available in paginated results
+
+    if (index === -1) {
+      console.debug(`No items matching ${this.fileHash}; requesting directly from API`);
+      // eslint-disable-next-line vue/no-async-in-computed-properties
+      apiSearch(this.$props.fileHash, this.$props.fileType)
+        .then((results) => {
+          if (results.error) throw results.error;
+          console.debug('results', results);
+          this.$data.item = results?.hits[0];
+        })
+        .catch(console.error);
+    }
+    */
   },
   props: {
     fileType: {
@@ -119,6 +137,7 @@ export default {
   data() {
     return {
       item: null,
+      carouselIndex: -1,
     };
   },
   computed: {
@@ -139,53 +158,24 @@ export default {
       }
     },
     items() {
-      return this.$store.state.results[this.fileType].results.hits;
-    },
-    carouselIndex: {
-      get() {
-        const index = this.items.findIndex((item) => item.hash === this.fileHash);
-        if (index === -1) {
-          console.debug(`No items matching ${this.fileHash}; requesting directly from API`);
-          // eslint-disable-next-line vue/no-async-in-computed-properties
-          apiSearch(this.$props.fileHash, this.$props.fileType)
-            .then((results) => {
-              if (results.error) throw results.error;
-              console.debug('results', results);
-              this.$data.item = results?.hits[0];
-            })
-            .catch(console.error);
-        }
-        return index;
-      },
-      set(index) {
-        // TODO: Fix issues with duplicate results
-        this.$router.replace({
-          ...this.$route,
-          query: {
-            ...this.$route.query,
-            page: this.primaryPage + Math.floor(index / this.$store.state.results[this.fileType].results.page_size),
-          },
-          params: {
-            ...this.$route.params,
-            fileHash: this.items[index].hash,
-          },
-        });
-      },
+      return store.state.results[this.fileType].results.hits;
     },
     detailComponent() {
-      return this.$store.state.query.type;
+      return store.state.query.type;
     },
   },
   watch: {
     carouselIndex: {
       handler(index) {
+        // load next page if end of page is reached
+        // TODO: refactor getResults to have more consistent behavior and update this watch
         const page = Number(this.$route.query.page);
         if (index === this.items.length - 1) {
-          console.debug('last item: loading items for page', page + 1);
-          this.$store.dispatch(`results/${this.$props.fileType}/getResults`, page + 1);
+          console.debug('last carousel item: loading items for page', page + 1);
+          store.dispatch(`results/${this.$props.fileType}/getResults`, page + 1);
         } else if (index === 0 && page > 1) {
-          console.debug('first item: loading items for page', page - 1);
-          this.$store.dispatch(`results/${this.$props.fileType}/getResults`, {
+          console.debug('first carousel item: loading items for page', page - 1);
+          store.dispatch(`results/${this.$props.fileType}/getResults`, {
             page: page - 1,
             prepend: true,
           }).then(() => {
@@ -197,6 +187,21 @@ export default {
     },
   },
   methods: {
+    onCarouselIndexChange(index) {
+      // change the fileHash and the page number in the url
+      console.debug('carousel index change: update url');
+      this.$router.replace({
+        ...this.$route,
+        query: {
+          ...this.$route.query,
+          page: this.primaryPage + Math.floor(index / store.state.results[this.fileType].results.page_size),
+        },
+        params: {
+          ...this.$route.params,
+          fileHash: this.items[index].hash,
+        },
+      });
+    },
     goHome() {
       this.$router.push({ path: '/' });
     },
