cfb39e22ee4c2014eb9c8224001f3f97513c637b
Mathijs de Bruin <mathijs@visualspace.nl>
2021-12-10 16:57:14 +0000

    Download fixups (#120)
    * Use mime library to resolve extensions.
    
    Closes #117, #119.
    
    * Tweaks in download button.
    
    Mark download links as downloads, include ext.

--- src/helpers/fileHelper.js
+++ src/helpers/fileHelper.js
@@ -1,39 +1,53 @@
-/**
- * Note that the npm-package 'mime-types' is not helpful here, because it turns 'audio/mpeg' into
- * mpga and that does not work for howler audio player. In the cases I've tried at least.
- *
- * @type {{'audio/mp3': string, 'audio/mpeg': string}}
- */
-// TODO: add more mimetype mappings
-// Howler supports:  "mp3", "mpeg", "opus", "ogg", "oga", "wav", "aac", "caf", "m4a", "m4b",
+const mime = require('mime/lite');
+
+// Howler (audio player) requires the following extensions:
+// "mp3", "mpeg", "opus", "ogg", "oga", "wav", "aac", "caf", "m4a", "m4b",
 // "mp4", "weba", "webm", "dolby", "flac"
-const mimetypeMap = {
-  'audio/mpeg': 'mp3',
-  'audio/mp3': 'mp3',
-  'audio/vorbis': 'ogg',
-  'audio/mp4': 'mp4',
-  'video/mp4': 'mp4',
-  'audio/aac': 'aac',
-  'audio/opus': 'opus',
-  'audio/wav': 'wav',
-  'audio/vnd.wave': 'wav',
-  'application/pdf': 'pdf',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
-  // omitting weba and webm because they have the same mimetype
-};
+//
+// Hence, we override 'audio/mpeg' to mp3 (defaults to mpga).
+//
+// Legacy types, for reference (they might turn out to need overriding as well).
+// const mimetypeMap = {
+//   'audio/mpeg': 'mp3',
+//   'audio/mp3': 'mp3',
+//   'audio/vorbis': 'ogg',
+//   'audio/mp4': 'mp4',
+//   'video/mp4': 'mp4',
+//   'audio/aac': 'aac',
+//   'audio/opus': 'opus',
+//   'audio/wav': 'wav',
+//   'audio/vnd.wave': 'wav',
+//   'application/pdf': 'pdf',
+//   'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+//   // omitting weba and webm because they have the same mimetype
+// };
+
+mime.define({
+  'audio/mpeg': ['mp3'],
+}, true);
 
 /**
- * Get file extension based on mimetype, or if inconclusive, the title of the file.
+ * Get file extension based on mimetype, or if inconclusive, the references of the file.
+ * If no extension is found, returns an empty string.
  * @param file
  * @returns {string|string|*}
  */
-// TODO: try to get file type by file header
 export function getFileExtension(file) {
-  if (file.mimetype && mimetypeMap[file.mimetype]) return mimetypeMap[file.mimetype];
-  const extractFromTitle = /(?:\.([^.]+))?$/
-    .exec(file.title)[1]
-    .replace(/<[^>]+>/g, '');// remove xml tags
-  return extractFromTitle || '';
+  // Mimetype is leading
+  if (file.mimetype) {
+    const ext = mime.getExtension(file.mimetype);
+    if (ext) return ext;
+  }
+
+  const filename = file.references?.[0]?.name;
+  if (filename) {
+    // Get filename extension, dealing with edge cases
+    // https://www.jstips.co/en/javascript/get-file-extension/
+    /* eslint no-bitwise: ["error", { "allow": [">>>"] }] */
+    return filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2);
+  }
+
+  return '';
 }
 
 export default {
