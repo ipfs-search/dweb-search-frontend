fff04c7541ba5ebca50a78201df7ac239f9b5a5a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-02-13 18:37:02 +0000

    Restore original Vue3 versions, but Vuetify them.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-loadFonts()
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+import App from "./App.vue";
+import router from "./router";
+
+loadFonts();
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
