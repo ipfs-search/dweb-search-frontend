91e9a32bf2d2f491fe9dcce1dc60bcd6010b75d5
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-02-13 18:28:13 +0000

    Vuetify 3 alpha

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-import App from "./App.vue";
-import router from "./router";
+loadFonts()
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
