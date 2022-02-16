c919113d64ef8d1c24b138df4dc57bd9f3c5e837
Frido Emans <verderster@gmail.com>
2022-02-16 16:35:41 +0100

    add comment

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -147,7 +147,9 @@ export default {
       // This is necessary for hiding the soft keyboard on iPhone
       // see v-closable="{ handler: 'onClick' }" in v-text-field
       // https://medium.com/@Taha_Shashtari/the-easy-vue-solution-to-dismiss-ios-keyboard-on-outside-click-2bb8be3c3347
+      // Needs to be conditional; otherwise, (some) non-iphone devices will cause console errors
       this.$refs?.input?.blur();
+
     },
   },
 };
