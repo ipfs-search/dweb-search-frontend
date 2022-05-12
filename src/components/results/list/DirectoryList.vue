9d870abd1f4343680e380fc8b5e5243bcae61e66
oszka <oszka@protonmail.com>
2022-05-12 14:24:23 +0100

    adding highlights on hover

--- src/components/results/list/DirectoryList.vue
+++ src/components/results/list/DirectoryList.vue
@@ -11,29 +11,32 @@
       v-for="(hit, index) in shownHits"
       :key="index"
     >
-      <v-card
-        @click="goToDetailPage(index)"
-      >
-        <v-card-subtitle class="text-caption mb-n7 text-truncate">
-          <span
-            :class="`${$options.filters.durationToColor(hit['last-seen'])}`"
-          >
-            &#9679;
-          </span>
-          <span v-if="hit['last-seen']">
-            Last seen <timeago :datetime="hit['last-seen']" />
-          </span><br>
-          <span v-if="hit.size">Size {{ hit.size | prettyBytes }}</span>
-        </v-card-subtitle>
-        <v-card-title
-          class="text-subtitle-1"
-          v-html="hit.title"
-        />
-        <v-card-subtitle
-          class="text-body-2"
-          v-html="hit.description"
-        />
-      </v-card>
+      <v-hover v-slot:default="{ hover }">
+        <v-card
+          @click="goToDetailPage(index)"
+          :elevation="hover ? 12 : 2"
+        >
+          <v-card-subtitle class="text-caption mb-n7 text-truncate">
+            <span
+              :class="`${$options.filters.durationToColor(hit['last-seen'])}`"
+            >
+              &#9679;
+            </span>
+            <span v-if="hit['last-seen']">
+              Last seen <timeago :datetime="hit['last-seen']" />
+            </span><br>
+            <span v-if="hit.size">Size {{ hit.size | prettyBytes }}</span>
+          </v-card-subtitle>
+          <v-card-title
+            class="text-subtitle-1"
+            v-html="hit.title"
+          />
+          <v-card-subtitle
+            class="text-body-2"
+            v-html="hit.description"
+          />
+        </v-card>
+      </v-hover>
     </v-col>
   </ListBase>
 </template>
@@ -44,9 +47,7 @@ import { Types } from '@/helpers/typeHelper';
 import FileListMixin from './mixins/FileListMixin';
 
 export default {
-  mixins: [
-    FileListMixin,
-  ],
+  mixins: [FileListMixin],
   filters: {
     durationToColor,
   },
