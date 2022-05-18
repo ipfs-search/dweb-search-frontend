13132eccd02a2792ac7c8a0f7d34a000d0f6d3fc
Frido Emans <verderster@gmail.com>
2022-05-18 21:50:33 +0200

    cleanup

--- tests/unit/store/query.toApi.spec.js
+++ tests/unit/store/query.toApi.spec.js
@@ -1,17 +1,3 @@
-/*
-Spec def:
-
-- we get a list of filters in the state (getter) (this because it is state-dependant)
-- the filters' definition is based on the state (getter)
-- the values of the filters is stored in the queryparams (and in the state)
-- every filter has a unique slug/identifier.
-- filters in the queryparams that are non-applicable are still persisted in the store, but not queried
-- a filter definition has a transformer method to turn its selection into an API query
-- a filter instance indicates, how to display its component
-- there are different types of filters, such as multiple select and single select;
-- it should be easy to extend/modify behaviour
-
-*/
 import { createStore } from '@/store';
 import query from '@/store/modules/query';
 import mockFilters from '../../mock-data';
