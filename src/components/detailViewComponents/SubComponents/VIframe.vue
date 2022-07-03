a6e8a13a3c7590f64c9645c0ec78721ff526689e
Frido Emans <verderster@gmail.com>
2022-07-03 08:23:00 +0200

    moved iframe styling to style section

--- src/components/detailViewComponents/SubComponents/VIframe.vue
+++ src/components/detailViewComponents/SubComponents/VIframe.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-let iframeLoading = ref(true);
+const iframeLoading = ref(true);
 const props = defineProps({
   src: {
     type: String,
@@ -8,24 +8,26 @@ const props = defineProps({
   }
 })
 </script>
+
 <template>
-  <div style="position: relative">
-    <div
-      style="position: absolute; text-align: center; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-      v-if="iframeLoading"
-    >
-      <v-progress-circular
-        indeterminate
-        size="90"
-        color="ipfsPrimary-lighten-4"
-      >
+  <div id="iframe-container">
+    <div id="iframe-spinner" v-if="iframeLoading">
+      <v-progress-circular indeterminate size="90" color="ipfsPrimary-lighten-4">
         <i>Loading...</i>
       </v-progress-circular>
     </div>
-    <iframe
-      style="width: 100%; height: calc(100vh - 200px);border: 1px solid #BDBDBD;"
-      @load="iframeLoading = false"
-      :src="src"
-    />
+    <iframe @load="iframeLoading = false" :src="src"/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+iframe {
+  width: 100%; height: calc(100vh - 200px);border: 1px solid #BDBDBD;
+}
+#iframe-container {
+  position: relative;
+}
+#iframe-spinner {
+  position: absolute; text-align: center; top: 50%; left: 50%; transform: translate(-50%, -50%);
+}
+</style>
