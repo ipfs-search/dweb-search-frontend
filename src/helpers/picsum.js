5632018de3e010c5603a1bdbbb3965ffabf6e2f7
Frido Emans <verderster@gmail.com>
2022-07-26 12:13:18 +0200

    refactor picsum for readability

--- src/helpers/picsum.js
+++ src/helpers/picsum.js
@@ -1,5 +1,7 @@
 export const picsum = ({ width = 200, height = 200, ...opts }) => {
-  const param = opts.seed ? `/seed/${opts.seed}` : opts.id ? `/id/${opts.id}` : "";
+  let param;
+  if (opts.seed) param = `/seed/${opts.seed}`;
+  if (opts.id) param = `/id/${opts.id}`;
   const p = `https://picsum.photos${param}/${width}/${height}`;
   const query = [];
   if (opts.random) query.push("random=" + Math.random());
