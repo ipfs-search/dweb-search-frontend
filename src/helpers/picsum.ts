45f5fbe9bcb75a8b6d5abeb0147e72c584580f68
Frido Emans <verderster@gmail.com>
2022-07-28 13:01:47 +0200

    converted picsum to TS and fixed TS support

--- src/helpers/picsum.ts
+++ src/helpers/picsum.ts
@@ -1,10 +1,20 @@
-export const picsum = (options) => {
-  const opts = { width: 200, height: 200, ...options };
+interface PicsumOptions {
+  width?: number;
+  height?: number;
+  seed?: string;
+  id?: number;
+  random?: boolean;
+  grayscale?: boolean;
+  blur?: number;
+}
+
+export const picsum = (options: PicsumOptions): string => {
+  const opts: PicsumOptions = { width: 200, height: 200, ...options };
   let param = "";
   if (opts.seed) param = `/seed/${opts.seed}`;
   if (opts.id) param = `/id/${opts.id}`;
   const p = `https://picsum.photos${param}/${opts.width}/${opts.height}`;
-  const query = [];
+  const query: string[] = [];
   opts.random && query.push("random=" + Math.random());
   opts.grayscale && query.push("grayscale");
   opts.blur && query.push("blur=" + opts.blur);
