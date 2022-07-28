1fa3ed7edd74a9c3941483452c26eb256c8d1643
Frido Emans <verderster@gmail.com>
2022-07-28 12:29:19 +0200

    test and fix picsum module

--- src/helpers/picsum.js
+++ src/helpers/picsum.js
@@ -1,8 +1,9 @@
-export const picsum = ({ width = 200, height = 200, ...opts }) => {
-  let param;
+export const picsum = (options) => {
+  const opts = { width: 200, height: 200, ...options };
+  let param = "";
   if (opts.seed) param = `/seed/${opts.seed}`;
   if (opts.id) param = `/id/${opts.id}`;
-  const p = `https://picsum.photos${param}/${width}/${height}`;
+  const p = `https://picsum.photos${param}/${opts.width}/${opts.height}`;
   const query = [];
   opts.random && query.push("random=" + Math.random());
   opts.grayscale && query.push("grayscale");
