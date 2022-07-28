44e52e1be8436f60af51fa03b1e78b75f27eed37
Frido Emans <verderster@gmail.com>
2022-07-28 11:51:42 +0200

    fix codeclimate

--- src/helpers/picsum.js
+++ src/helpers/picsum.js
@@ -4,8 +4,8 @@ export const picsum = ({ width = 200, height = 200, ...opts }) => {
   if (opts.id) param = `/id/${opts.id}`;
   const p = `https://picsum.photos${param}/${width}/${height}`;
   const query = [];
-  if (opts.random) query.push("random=" + Math.random());
-  if (opts.grayscale) query.push("grayscale");
-  if (opts.blur) query.push("blur=" + opts.blur);
+  opts.random && query.push("random=" + Math.random());
+  opts.grayscale && query.push("grayscale");
+  opts.blur && query.push("blur=" + opts.blur);
   return `${p}${query.length ? "?" + query.join("&") : ""}`;
 };
