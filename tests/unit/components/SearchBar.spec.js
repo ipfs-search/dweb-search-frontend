433eb1340c8ad38f17a81b7bb3284480395712dc
Frido Emans <verderster@gmail.com>
2022-08-02 15:18:32 +0200

    fix unit tests

--- tests/unit/components/SearchBar.spec.js
+++ tests/unit/components/SearchBar.spec.js
@@ -1,16 +1,16 @@
 import store from "@/store";
 import SearchBar from "@/components/pageLayout/SearchBar.vue";
-import { Types } from "@/helpers/typeHelper";
+import { TypeListNames, Types } from "@/helpers/typeHelper";
 import { localMount } from "../../vitest-helpers";
 
 describe("SearchBar", () => {
   it("renders a filetype select filter", async () => {
     const wrapper = localMount(SearchBar, { store });
     const typeValue = wrapper.get('[data-testid="type-filter-selector-value"]');
-    expect(typeValue.text().toLowerCase()).toBe(Types.all);
+    expect(typeValue.text()).toBe(TypeListNames.all);
 
     store.commit("query/filters/type/setValue", Types.text);
     await wrapper.trigger("change"); // wait for the store value to propagate in the component
-    expect(typeValue.text().toLowerCase()).toBe(Types.text);
+    expect(typeValue.text()).toBe(TypeListNames.text);
   });
 });
