97ddeedcf1590a769175dc24a601151bbe8a5ff4
Frido Emans <verderster@gmail.com>
2022-09-17 22:51:40 +0200

    responsive alignment of searchview

--- src/components/searchViewComponents/ImageList.vue
+++ src/components/searchViewComponents/ImageList.vue
@@ -2,15 +2,12 @@
 import NsfwTooltip from "@/components/shared/nsfwTooltip.vue";
 import ListBase from "./BaseList.vue";
 import HoverCard from "./subcomponents/HoverCard.vue";
-import { useFileListComposable, imports } from "@/composables/useFileListComposable";
+import TileList from "@/components/searchViewComponents/TileList.vue";
+import { imports } from "@/composables/useFileListComposable";
 import { useBlurExplicit } from "@/composables/BlurExplicitImagesComposable";
 import { Types } from "@/helpers/typeHelper";
-import { useDisplay } from "vuetify";
 
 const fileType = Types.images;
-const { xs, smAndDown, mdAndDown } = useDisplay();
-
-const { slicedHits } = useFileListComposable({ fileType });
 
 const { getResourceURL } = imports;
 
@@ -19,33 +16,24 @@ const { blurExplicit } = useBlurExplicit();
 
 <template>
   <ListBase :file-type="fileType">
-    <v-row dense>
-      <v-col
-        v-for="(hit, index) in slicedHits(xs ? 2 : smAndDown ? 6 : mdAndDown ? 8 : 12)"
-        :key="index"
-        cols="6"
-        sm="4"
-        md="3"
-        lg="2"
-      >
-        <hover-card :hit="hit" :index="index" :file-type="fileType">
-          <v-img
-            :src="getResourceURL(hit.hash)"
-            aspect-ratio="1"
-            :class="{ blurExplicit: blurExplicit(hit) }"
-            :data-nsfw-classification="JSON.stringify(hit.nsfwClassification)"
-            :data-nsfw="hit.nsfw"
-            class="rounded grey lighten-2"
-          >
-            <template #placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular indeterminate color="ipfsPrimary" />
-              </v-row>
-            </template>
-            <NsfwTooltip :file="hit" />
-          </v-img>
-        </hover-card>
-      </v-col>
-    </v-row>
+    <tile-list v-slot="{ hit, index }" :file-type="fileType">
+      <hover-card :hit="hit" :index="index" :file-type="fileType">
+        <v-img
+          :src="getResourceURL(hit.hash)"
+          aspect-ratio="1"
+          :class="{ blurExplicit: blurExplicit(hit) }"
+          :data-nsfw-classification="JSON.stringify(hit.nsfwClassification)"
+          :data-nsfw="hit.nsfw"
+          class="rounded grey lighten-2"
+        >
+          <template #placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular indeterminate color="ipfsPrimary" />
+            </v-row>
+          </template>
+          <NsfwTooltip :file="hit" />
+        </v-img>
+      </hover-card>
+    </tile-list>
   </ListBase>
 </template>
---
+++ src/components/searchViewComponents/TileList.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useDisplay } from "vuetify";
+
+const { xs, smAndDown, mdAndDown, lgAndUp } = useDisplay();
+const props = defineProps({
+  fileType: {
+    type: String,
+    required: true,
+  },
+});
+import { useFileListComposable } from "@/composables/useFileListComposable";
+const { slicedHits } = useFileListComposable({ fileType: props.fileType });
+</script>
+<template>
+  <v-row dense>
+    <v-col>
+      <v-row dense>
+        <v-col
+          v-for="(hit, index) in slicedHits(xs ? 2 : smAndDown ? 6 : mdAndDown ? 8 : 12)"
+          :key="index"
+          cols="6"
+          sm="4"
+          md="3"
+          :class="{ five_columns: lgAndUp }"
+        >
+          <slot :hit="hit" :index="index" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.five_columns {
+  width: 20%;
+  max-width: 20%;
+  flex-basis: 20%;
+}
+</style>
