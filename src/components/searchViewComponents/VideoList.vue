f77ac60783e49981e3c3765b8488283e3668aa52
Frido Emans <verderster@gmail.com>
2022-09-22 21:47:57 +0200

    small fix in videolist component

--- src/components/searchViewComponents/VideoList.vue
+++ src/components/searchViewComponents/VideoList.vue
@@ -16,15 +16,21 @@ const { slicedHits } = useFileListComposable({ fileType });
 <template>
   <ListBase :file-type="fileType">
     <v-row dense>
-      <v-col v-for="(hit, index) in slicedHits(3)" :key="index" cols="12">
-        <hover-card :hit="hit" :index="index" :file-type="fileType">
+      <v-col cols="12">
+        <hover-card
+          v-for="(hit, index) in slicedHits(3)"
+          :key="index"
+          :hit="hit"
+          :index="index"
+          :file-type="fileType"
+        >
           <v-row>
             <v-col cols="12" sm="4" md="3" lg="2">
               <v-img
                 class="ma-3 mr-sm-0"
                 cover
                 aspect-ratio="1.778"
-                :src="hit.src || picsum({ seed: hit.hash })"
+                :src="picsum({ seed: hit.hash })"
                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               >
                 <media-center-icon :icon="mdiVideo" />
