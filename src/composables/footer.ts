60b1941aba7d87eec47b44d76169fd32f73abaed
Frido Emans <verderster@gmail.com>
2022-09-23 15:30:23 +0200

    simplify some code

--- src/composables/footer.ts
+++ src/composables/footer.ts
@@ -6,20 +6,23 @@ export const _footerVisible = ref(true);
 
 export const useFooter = () => {
   const route = useRoute();
+
   const footerVisible = computed(() => {
     if (audioPlayer.value.file || playlistVisible.value || audioDetailPopup.value) return false;
     if (route.name === "Home") return true;
     return _footerVisible.value && route.name === "Search";
   });
+
   const hideFooter = () => {
     _footerVisible.value = false;
   };
+
   const adjustFooterPadding = computed(() => {
-    if (route.name === "Home" && !playlistVisible.value && !audioDetailPopup.value) return "0px";
     if (audioPlayer.value.file) return "100px";
     if (footerVisible.value) return "120px";
     return "0px";
   });
+
   return {
     footerVisible,
     hideFooter,
