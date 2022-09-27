8b2ca43eb9b0b54a90b5a4c7d32fd6cedfa8868e
Frido Emans <verderster@gmail.com>
2022-09-27 16:33:16 +0200

    clean up midi event handler function

--- src/helpers/midiPlayer.ts
+++ src/helpers/midiPlayer.ts
@@ -41,26 +41,6 @@ export class Midi implements IMediaPlayer {
   private trackNames: Record<number, string>;
   private midiPlayer;
   private loaded = false;
-  private handleMidiEvent = (event: IMidiEvent) => {
-    if (event.name === "Sequence/Track Name") {
-      this.trackNames[event.track] = event.string?.toLowerCase();
-    } else if (event.name === "Note on" && event.velocity > 0) {
-      if (!this.patchBay[event.track]) {
-        // eslint-disable-next-line no-nested-ternary
-        this.patchBay[event.track] = this.trackNames[event.track]?.includes("drum")
-          ? instrumentList.length - 1
-          : this.trackNames[event.track]?.includes("percussion")
-          ? instrumentList.length - 2
-          : Math.floor(Math.random() * (instrumentList.length - 2));
-      }
-      // if (!patchBay.includes(event.track)) patchBay.push(event.track);
-      instruments[this.patchBay[event.track]]?.play(event.noteName, this.audioContext.currentTime, {
-        gain: event.velocity / 100,
-      });
-    } else if (event.name === "Note off" || (event.name === "Note on" && event.velocity === 0)) {
-      instruments[this.patchBay[event.track]]?.stop();
-    }
-  };
   private autoplay: boolean;
 
   constructor(options: { context: IAudio; file: IFile; autoplay: boolean }) {
@@ -70,7 +50,7 @@ export class Midi implements IMediaPlayer {
     this.audioContext = new AudioContext();
     this.patchBay = [];
     this.trackNames = [];
-    this.midiPlayer = new MidiPlayer.Player(this.handleMidiEvent);
+    this.midiPlayer = new MidiPlayer.Player(this.handleMidiEvent.bind(this));
     this.load();
     this.once("loaderror", (message) => {
       this.context.reportError(this.file.hash, `Load Error: ${message}`);
@@ -90,6 +70,58 @@ export class Midi implements IMediaPlayer {
     this.midiPlayer.on("endOfFile", () => this.callEvent("end"));
   }
 
+  private patchTrack(track: number) {
+    if (!this.patchBay[track]) {
+      if (this.trackNames[track]?.includes("drum")) {
+        this.patchBay[track] = instrumentList.length - 1;
+        console.log(
+          `patched track ${track} - ${this.trackNames[track]} to ${
+            instrumentList[instrumentList.length - 1]
+          }`
+        );
+      } else if (this.trackNames[track]?.includes("percussion")) {
+        this.patchBay[track] = instrumentList.length - 2;
+        console.log(
+          `patched track ${track} - ${this.trackNames[track]} to ${
+            instrumentList[instrumentList.length - 2]
+          }`
+        );
+      } else {
+        this.patchBay[track] = Math.floor(Math.random() * (instrumentList.length - 2));
+        console.log(
+          `patched track ${track} - ${this.trackNames[track]} of ${this.file.title} to ${
+            instrumentList[this.patchBay[track]]
+          }`
+        );
+      }
+    }
+    return this.patchBay[track];
+  }
+
+  private handleMidiEvent(event: IMidiEvent) {
+    switch (event.name) {
+      case "Sequence/Track Name":
+        this.trackNames[event.track] = event.string?.toLowerCase();
+        break;
+      case "Note on":
+        if (event.velocity > 0) {
+          instruments[this.patchTrack(event.track)]?.play(
+            event.noteName,
+            this.audioContext.currentTime,
+            {
+              gain: event.velocity / 100,
+            }
+          );
+        } else {
+          instruments[this.patchTrack(event.track)]?.stop();
+        }
+        break;
+      case "Note off":
+        instruments[this.patchTrack(event.track)]?.stop();
+        break;
+    }
+  }
+
   play() {
     if (this.loaded) {
       this.context.playing = true;
@@ -100,6 +132,7 @@ export class Midi implements IMediaPlayer {
       this.callEvent("play");
     }
   }
+
   pause() {
     if (this.loaded) {
       this.midiPlayer.pause();
@@ -130,6 +163,32 @@ export class Midi implements IMediaPlayer {
       this.onceEventStore[event] = [];
       this.onEventStore[event] = [];
     });
+  }
+
+  async load() {
+    abortController?.abort("loading a new file");
+    this.midiPlayer?.stop();
+    try {
+      this.loaded = false;
+      this.context.loading = true;
+      this.context.loaded = false;
+      if (!instruments.length) {
+        instruments = await Promise.all([
+          ...instrumentList.map((instrument) =>
+            Soundfont.instrument(this.audioContext, instrument)
+          ),
+        ]);
+      }
+      abortController = new AbortController();
+      fetch(getResourceURL(this.file.hash), { signal: abortController.signal })
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.arrayBuffer().then((buffer) => this.midiPlayer.loadArrayBuffer(buffer));
+        })
+        .catch((error) => this.callEvent("loaderror", error));
+    } catch (error: unknown) {
+      this.callEvent("loaderror");
+    }
   }
 
   unload() {
@@ -171,31 +230,5 @@ export class Midi implements IMediaPlayer {
 
   once(event: MediaPlayerEvent, callback: (...args: unknown[]) => void) {
     this.onceEventStore[event].push(callback);
-  }
-
-  async load() {
-    abortController?.abort("loading a new file");
-    this.midiPlayer?.stop();
-    try {
-      this.loaded = false;
-      this.context.loading = true;
-      this.context.loaded = false;
-      if (!instruments.length) {
-        instruments = await Promise.all([
-          ...instrumentList.map((instrument) =>
-            Soundfont.instrument(this.audioContext, instrument)
-          ),
-        ]);
-      }
-      abortController = new AbortController();
-      fetch(getResourceURL(this.file.hash), { signal: abortController.signal })
-        .then((response) => {
-          if (!response.ok) throw Error(response.statusText);
-          return response.arrayBuffer().then((buffer) => this.midiPlayer.loadArrayBuffer(buffer));
-        })
-        .catch((error) => this.callEvent("loaderror", error));
-    } catch (error: unknown) {
-      this.callEvent("loaderror");
-    }
   }
 }
