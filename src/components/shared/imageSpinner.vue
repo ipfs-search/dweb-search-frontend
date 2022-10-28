302f8ae791a7fc2d63f3ee84d721ea8d91e18a38
Frido Emans <verderster@gmail.com>
2022-10-28 15:26:07 +0200

    happy logo spinner

--- src/components/shared/imageSpinner.vue
+++ src/components/shared/imageSpinner.vue
@@ -1,7 +1,19 @@
 <template>
   <v-row class="fill-height ma-0" align="center" justify="center">
-    <v-img
-      src="https://icon-library.com/images/loading-icon-animated-gif/loading-icon-animated-gif-6.jpg"
-    />
+    <v-img class="spinner" src="/assets/logo-hexagon-black.png" />
   </v-row>
 </template>
+
+<style scoped>
+.spinner {
+  width: 50%;
+  height: 50%;
+  max-width: 120px;
+  animation: spin 4s linear infinite;
+}
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
