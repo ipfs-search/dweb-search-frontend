e3d95a8d2d2ccf6bb10db7595035805a4c973d61
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-10-30 15:00:56 +0000

    Deploy IPFS Camp II (#280)
    * Responsively hide footer.
    
    Closes #278.
    
    * reduce function length for codeclimate
    
    Co-authored-by: Frido Emans <verderster@gmail.com>

--- src/composables/footer.ts
+++ src/composables/footer.ts
@@ -2,20 +2,28 @@ import { ref, computed } from "vue";
 import { useRoute } from "vue-router";
 
 import { playlistVisible, audioPlayer, audioDetailPopup } from "@/composables/audioControls";
+import { useDisplay } from "vuetify";
+
 export const _footerVisible = ref(true);
+const hideFooter = () => {
+  _footerVisible.value = false;
+};
 
 export const useFooter = () => {
   const route = useRoute();
+  const { height } = useDisplay();
 
   const footerVisible = computed(() => {
-    if (audioPlayer.value.file || playlistVisible.value || audioDetailPopup.value) return false;
+    if (
+      audioPlayer.value.file ||
+      playlistVisible.value ||
+      audioDetailPopup.value ||
+      height.value < 600
+    )
+      return false;
     if (route.name === "Home") return true;
     return _footerVisible.value && route.name === "Search";
   });
-
-  const hideFooter = () => {
-    _footerVisible.value = false;
-  };
 
   const adjustFooterPadding = computed(() => {
     if (audioPlayer.value.file) return "100px";
