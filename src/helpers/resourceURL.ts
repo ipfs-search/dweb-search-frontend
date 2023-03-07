a8832ea05552e88eb191915604e26a5688fda907
Frido Emans <frido@ipfs-search.com>
2023-03-07 13:03:25 +0100

    Use nativeIpfs query param to indicate to use ipfs:// urls

--- src/helpers/resourceURL.ts
+++ src/helpers/resourceURL.ts
@@ -1,6 +1,11 @@
+import router from "@/router";
+
 const gatewayURL = "https://gateway.ipfs.io";
 
 function getResourceURL(hash: string) {
+  if(router?.currentRoute.value.query?.nativeIpfs) {
+    return `ipfs://${hash}`
+  }
   return `${gatewayURL}/ipfs/${hash}`;
 }
 
