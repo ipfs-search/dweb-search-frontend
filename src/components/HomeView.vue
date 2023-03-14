7e7dff369567db30b41e231a0fda65a72f2b57f7
Frido Emans <frido@ipfs-search.com>
2023-03-14 17:59:38 +0100

    implemented questionnaire banner

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,8 +1,12 @@
-<script setup>
+<script setup lang="ts">
+import {ref} from 'vue'
 import SearchBar from "@/components/pageLayout/SearchBar.vue";
-import { onMounted } from "vue";
+import { onBeforeMount } from "vue";
 import { _footerVisible } from "@/composables/footer";
-onMounted(() => {
+import { mdiHeart } from "@mdi/js";
+import store from '@/store'
+
+onBeforeMount(() => {
   _footerVisible.value = true;
 });
 </script>
@@ -12,9 +16,39 @@ onMounted(() => {
     <v-container class="pa-0 fill-height d-flex mt-12">
       <v-row class="align-content-center">
         <v-col class="pa-2">
-          <h3 class="mb-2 text-center hidden-sm-and-down text-white">
-            Searching the Universe since 2016
-          </h3>
+          <v-alert
+            v-if="store.state.localStorage.questionnaireBanner"
+            id="questionnaireBanner"
+            color="ipfsPrimary-lighten-1"
+            border
+            :icon="mdiHeart"
+            class="mt-3 w-100"
+            position="absolute"
+            location="top"
+            closable
+            type="info"
+            @update:model-value="store.commit('localStorage/setQuestionnaireBanner', false)"
+          >
+            <a
+              href="https://framaforms.org/help-us-improve-ipfs-search-1678325605"
+              target="_blank"
+              style="text-decoration: none; color: inherit"
+            >
+              Got a moment to help us enhance IPFS-Search? please take
+              this short <u>questionnaire.</u>
+            </a>
+          </v-alert>
+          <v-btn
+            v-else
+            :icon="mdiHeart"
+            color="ipfsPrimary-lighten-1"
+            position="absolute"
+            location="top right"
+            class="mt-3 mr-3"
+            @click="store.commit('localStorage/setQuestionnaireBanner', true)"
+          >
+          </v-btn>
+          <h3 class="mb-2 text-center text-white">Searching the Universe since 2016</h3>
           <SearchBar />
         </v-col>
       </v-row>
@@ -28,5 +62,9 @@ h3 {
   letter-spacing: -0.02em;
   font-size: 150%;
   font-weight: 700;
+}
+
+#questionnaireBanner {
+  max-width: 915px;
 }
 </style>
