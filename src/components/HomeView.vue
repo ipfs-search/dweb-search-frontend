311e2d7ce536e2195128e9abed3652ebdae6ba1a
Frido Emans <verderster@gmail.com>
2023-03-23 14:10:15 +0100

    Questionnaire banner (#293)
    * implemented questionnaire banner
    
    * swap link to cognitoform

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -30,7 +30,7 @@ onBeforeMount(() => {
             @update:model-value="store.commit('localStorage/setQuestionnaireBanner', false)"
           >
             <a
-              href="https://framaforms.org/help-us-improve-ipfs-search-1678325605"
+              href="https://www.cognitoforms.com/Ipfssearchcom/HelpUsImproveIPFSSearchCom"
               target="_blank"
               style="text-decoration: none; color: inherit"
             >
