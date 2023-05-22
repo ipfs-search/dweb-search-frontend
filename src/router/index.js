c017b663744e8e1828a5b95c9cb6fd46b13b8ce2
Frido Emans <frido@ipfs-search.com>
2023-05-22 14:37:10 +0200

    turn off other routes than home

--- src/router/index.js
+++ src/router/index.js
@@ -7,30 +7,30 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  {
-    path: "/search",
-    name: "Search",
-    props: ({ query }) => ({
-      query: {
-        ...query,
-        page: Number(query.page),
-      },
-    }),
-    component: () => import("@/components/SearchView.vue"),
-  },
-  {
-    path: "/search/detail/:fileType/:fileHash",
-    name: "Detail",
-    props: ({ params, query }) => ({
-      ...params,
-      query: {
-        ...query,
-        page: Number(query.page),
-      },
-      selectedIndex: Number(params.selectedIndex),
-    }),
-    component: () => import("@/components/DetailView.vue"),
-  },
+  // {
+  //   path: "/search",
+  //   name: "Search",
+  //   props: ({ query }) => ({
+  //     query: {
+  //       ...query,
+  //       page: Number(query.page),
+  //     },
+  //   }),
+  //   component: () => import("@/components/SearchView.vue"),
+  // },
+  // {
+  //   path: "/search/detail/:fileType/:fileHash",
+  //   name: "Detail",
+  //   props: ({ params, query }) => ({
+  //     ...params,
+  //     query: {
+  //       ...query,
+  //       page: Number(query.page),
+  //     },
+  //     selectedIndex: Number(params.selectedIndex),
+  //   }),
+  //   component: () => import("@/components/DetailView.vue"),
+  // },
 ];
 
 const router = createRouter({
